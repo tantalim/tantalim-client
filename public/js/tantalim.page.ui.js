30ef723a8d32f4a2b61e23265d4ac8332ee9f943
Trevor Allred <trevorallred@gmail.com>
2015-04-11 23:17:53 -0700

    improve the select in grids

--- public/js/tantalim.page.ui.js
+++ public/js/tantalim.page.ui.js
@@ -66,6 +66,45 @@ angular.module('tantalim.desktop')
         };
     });
 
+// Source: public/js/page/ui/grid.js
+angular.module('tantalim.desktop')
+    .directive('tntGrid', function () {
+        return {
+            restrict: 'E',
+            controllerAs: '$grid',
+            controller: function ($scope, $attrs) {
+
+                var ctrl = this;
+                ctrl.help = $attrs.help;
+                ctrl.id = '123';
+                ctrl.width = 200;
+
+                var targetField = $attrs.targetField;
+                var required = $attrs.required === 'true';
+                ctrl.toggle = function () {
+                    $scope.currentInstance.toggle(targetField, required);
+                    setValue($scope.currentInstance);
+                };
+
+                function setValue(instance) {
+                    if (instance) {
+                        ctrl.value = instance.data[targetField];
+                    } else {
+                        ctrl.value = null;
+                    }
+                }
+
+                $scope.$watch('currentInstance', setValue);
+            },
+            transclude: true,
+            template: '<div class="tnt-grid tnt-grid-{{$grid.id}}">' +
+            '<style> .tnt-grid-{{$grid.id}} { width: {{$grid.width}}px; }</style>{{$grid.id}}' +
+            '' +
+            '</div>'
+        };
+    })
+;
+
 // Source: public/js/page/ui/help.js
 angular.module('tantalim.desktop')
     .directive('tntHelp', function () {
